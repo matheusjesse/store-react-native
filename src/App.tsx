2ad90718d55a18283146773a8602d10a37cac021
matheusjesse <matheus_jesse@hotmail.com>
2023-02-20 20:55:43 -0300

    build: dependencias de navegacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Oi!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
