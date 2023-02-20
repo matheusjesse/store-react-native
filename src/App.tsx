54a4ebf9ad0f2396d5ebe4c3974540c1c151fd66
matheusjesse <matheus_jesse@hotmail.com>
2023-02-20 20:35:41 -0300

    build: instalando styledcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oi!!a</Text>
+      <Text>Oi!!</Text>
       <StatusBar style="auto" />
     </View>
   );
