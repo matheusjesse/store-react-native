3772fb1278e79216aa438226545bc536b214dec6
matheusjesse <matheus_jesse@hotmail.com>
2023-02-20 20:49:05 -0300

    build: instalando dependencia de navegacao de tela

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
