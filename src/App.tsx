3cfb8443b87dd4c7db7b48d2029fa705cd6d16c6
matheusjesse <matheus_jesse@hotmail.com>
2023-02-20 21:13:10 -0300

    feat: configurando rota da tela home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './views/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-
-    </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
